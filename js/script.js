e1e2073263c0b77714825ce596c03035b3454074
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-06-28 00:29:42 +0530

    Carousel-updation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+(function ($) {
+  $(document).ready(function () {
+    $('.carouselbox').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      infinite: true,
+      prevArrow: $('.prev-arrow'),
+      nextArrow: $('.next-arrow'),
+    });
+  });
+})(jQuery);
