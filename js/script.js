46cf37c4ce3d0e7669fb9174f9fded9acbbfe887
Navu4 <51900952+Navu4@users.noreply.github.com>
2021-06-29 03:48:43 +0530

    Responsive Carousel, Stats and footer

--- js/script.js
+++ js/script.js
@@ -6,6 +6,33 @@
       infinite: true,
       prevArrow: $('.prev-arrow'),
       nextArrow: $('.next-arrow'),
+      responsive: [
+        {
+          breakpoint: 1135,
+          settings:{
+            slidesToShow: 5,
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings:{
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 500,
+          settings:{
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 380,
+          settings:{
+            slidesToShow: 1,
+          }
+        },
+      ]
+
     });
 
   });
